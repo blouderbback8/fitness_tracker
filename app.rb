a60195549cb4424ccbade47392ff581fa2958576
blouderbback8 <brianlouderback@student.olympic.edu>
2025-03-02 11:45:24 -0800

    Add 30-minute cardio rule and indulgence tracking
    - Added logic to enforce 30 minutes of cardio after indulgences
    - Blocked alcohol logging on Jiu-Jitsu days (Mon/Wed/Fri)
    - Prevented alcohol logging if indulgence was already logged on Tue/Thu/Sun
    - Updated index.erb to display cardio reminder when required

--- app.rb
+++ app.rb
@@ -29,10 +29,27 @@ DB.execute <<-SQL
   );
 SQL
 
+# Function to calculate extra workout time based on indulgences
+def calculate_extra_minutes
+  indulgences = DB.execute("SELECT SUM(calories) as total_calories FROM indulgences").first
+  total_calories = indulgences["total_calories"].to_i || 0
+  extra_minutes = total_calories / 50  # Example: burn 50 calories per extra workout minute
+  extra_minutes
+end
+
+# Function to check if cardio is required the next day
+def cardio_required_tomorrow?
+  yesterday = (Date.today - 1).to_s
+  indulgence = DB.execute("SELECT * FROM indulgences WHERE date = ?", [yesterday]).first
+  !indulgence.nil?  # If there's an indulgence from yesterday, cardio is required
+end
+
 # Route to show the home page
 get '/' do
   @workouts = DB.execute("SELECT * FROM workouts")
   @indulgences = DB.execute("SELECT * FROM indulgences")
+  @extra_minutes = calculate_extra_minutes  # Get calculated adjustments
+  @cardio_required = cardio_required_tomorrow?
 
   # Convert workouts to a hash grouped by date for calendar display
   @workouts_by_date = @workouts.group_by { |w| w["date"] }
@@ -49,8 +66,22 @@ post '/add_workout' do
   redirect '/'
 end
 
-# Route to add a new indulgence
+# Route to add a new indulgence (prevents alcohol on certain days)
 post '/add_indulgence' do
+  date = params[:date]
+  name = params[:name].downcase  # Normalize name for alcohol detection
+
+  # Prevent alcohol logging on Jiu-Jitsu days (Mon/Wed/Fri)
+  if ["monday", "wednesday", "friday"].include?(Date.parse(date).strftime("%A").downcase) && name.include?("alcohol")
+    return "Alcohol logging is not allowed on Jiu-Jitsu days!", 400
+  end
+
+  # Prevent alcohol logging if an indulgence was already logged on Tue/Thu/Sun
+  indulgence_logged = DB.execute("SELECT * FROM indulgences WHERE date = ?", [date]).any?
+  if indulgence_logged && name.include?("alcohol")
+    return "You cannot log alcohol on the same day as another indulgence!", 400
+  end
+
   DB.execute("INSERT INTO indulgences (name, calories, date) VALUES (?, ?, ?)",
              [params[:name], params[:calories], params[:date]])
   redirect '/'
